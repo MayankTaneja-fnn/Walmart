5fb61543cb033e1564751e447afaf3b192ddb8bb
Mayank Taneja <tanejamayank21@gmail.com>
2025-07-08 08:16:18 +0000

    added images

--- src/components/icons/walmart-logo.tsx
+++ src/components/icons/walmart-logo.tsx
@@ -1,15 +1,11 @@
 import * as React from 'react';
+import Image from 'next/image';
+import walmartLogo from '../../app/walmart-logo.png' // rename the uploaded file to this in your project
 
-// This is the Walmart Spark logo.
-export const WalmartLogo = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    width="1em"
-    height="1em"
-    viewBox="0 0 100 100"
-    fill="currentColor"
-    xmlns="http://www.w3.org/2000/svg"
+export const WalmartLogo = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
+  <Image
+    src={walmartLogo}
+    alt="Walmart Logo"
     {...props}
-  >
-    <path d="M83.43,44.82,68.61,38.34l-5-15.35a2.76,2.76,0,0,0-5.31,0l-5,15.35L38.48,44.82a2.76,2.76,0,0,0,0,2.76l14.82,6.48,5,15.35a2.76,2.76,0,0,0,5.31,0l5-15.35,14.82-6.48A2.76,2.76,0,0,0,83.43,44.82Z" />
-  </svg>
+  />
 );
